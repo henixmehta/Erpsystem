f7bfe0799c9c5028c56791ab79a6ffa074dbfddc
henixmehta <henumehta03@gmail.com>
2024-01-02 08:54:07 +0530

    validation

--- formvalidation.php
+++ formvalidation.php
@@ -1,5 +1,5 @@
 <?php
-$e_fname = $add1 = $e_lname = $e_email = $e_mobno = $e_altconno = $e_pno = $e_uname = $e_pass = $e_rpass = "";
+$e_fname = $e_lname = $e_add1 = $e_email = $e_mobno = $e_altconno = $e_pno = $e_uname = $e_pass = $e_rpass = "";
 
 if (isset($_POST['sub_btn'])) {
     // Validate First Name
@@ -9,10 +9,12 @@ if (isset($_POST['sub_btn'])) {
 
     // Validate Last Name
     if (empty($_POST['lname'])) {
-        $e_lname = "Enter Last Name";
+        $e_lname .= "Enter Last Name";
     }
+
+    // Validate Address 1
     if (empty($_POST['add1'])) {
-        $e_add1= "Enter Address";
+        $e_add1 .= "Enter Address";
     }
 
     // Validate Email
@@ -30,31 +32,43 @@ if (isset($_POST['sub_btn'])) {
     // Validate Mobile Number
     if (empty($_POST['mobno'])) {
         $e_mobno .= "Enter Mobile Number";
+    } else {
+        // Additional validation for mobile number if needed
     }
 
     // Validate Alternate Contact
     if (empty($_POST['altconno'])) {
         $e_altconno .= "Enter Alternate Contact";
+    } else {
+        // Additional validation for alternate contact if needed
     }
 
     // Validate Pin Code
     if (empty($_POST['pno'])) {
         $e_pno .= "Enter Pin Code";
+    } else {
+        // Additional validation for pin code if needed
     }
 
     // Validate User Name
     if (empty($_POST['uname'])) {
         $e_uname .= "Enter User Name";
+    } else {
+        // Additional validation for user name if needed
     }
 
     // Validate Password
     if (empty($_POST['pass'])) {
         $e_pass .= "Enter Password";
+    } else {
+        // Additional validation for password if needed
     }
 
     // Validate Repeat Password
     if (empty($_POST['rpass'])) {
         $e_rpass .= "Enter Repeat Password";
+    } else {
+        // Additional validation for repeat password if needed
     }
 
     // Additional validations can be added as needed
