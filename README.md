0d135df7a8e91b98308b9388b4050b564e618c44
docodeunknow <docodeparthiv@gmail.com>
2024-03-02 20:47:40 +0530

    Remove file

--- README.md
+++
@@ -1,3 +0,0 @@
-# Erpsystem
-
-git clone https://henixmehta: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@github.com/henixmehta/Erpsystem.git
--- special-pages/css.php
+++
@@ -1,3 +0,0 @@
-<link rel="stylesheet" href="Assets/css/main.min.css">
-<!-- ../../assets/css/main.min.css -->
-<link rel="stylesheet" href="Assets/css/login.css">
