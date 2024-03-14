8fbeaffc28bfa387003f398e86ba76056e0a0e70
henixmehta <henumehta03@gmail.com>
2024-03-15 02:25:21 +0530

    ..

--- team/updateteam.php
+++ team/updateteam.php
@@ -66,8 +66,8 @@ if (isset($_POST['sub_btn'])) {
         }
 
         // Allow certain file formats
-        if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-            && $imageFileType != "gif") {
+        if ($imageFileType == "jpg" && $imageFileType == "png" && $imageFileType == "jpeg"
+            && $imageFileType == "gif") {
             echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
             $uploadOk = 0;
         }
