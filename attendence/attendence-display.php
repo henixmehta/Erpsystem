6ac019078858c0e7e599131ddfe83999127902b8
henixmehta <henumehta03@gmail.com>
2024-03-14 15:46:12 +0530

    done

--- attendence/attendence-display.php
+++ attendence/attendence-display.php
@@ -1,106 +1,102 @@
-<?php 
-   include '../sidebar/sidebar.php';
-   include '../connection/connection.php';  
-   
+<?php
+include '../sidebar/sidebar.php';
+include '../connection/connection.php';
+
 ?>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Leaves</title>
-        <!-- <link rel="stylesheet" href="../css/main.min.css">
-        <link rel="stylesheet" href="../sidebar.css"> -->
-    </head>
-    <body>
-        <main class="main-content">
-            <div class="conatiner-fluid content-inner mt-n5 py-0">
-                <div class="row">
-                    <div class="col-xl-12 col-lg-8">
-                        <div class="card">
-                            <div class="card-header d-flex justify-content-between">
-                                <h4 class="card-title">
-                                    Attendence List
-                                </h4>
-                               
-                    <!-- set session -->
-                        <?php
-                        if($_SESSION['e_role']=="admin")
-                        {
-                            ?>
-                                <a href="leave-insert.php">
-                                    <button class="btn btn-primary">
-                                        Add
-                                    </button>
-                                </a>
-                            <?php
-                        }
-                         ?>
-                            </div>
-                            <div class="card-body">         
-                                <div class="card-body px-0">
-                                    <div class="table-responsive">
-                                        <div class="sidebar-body pt-0 data-scrollbar"style="max-height: calc(100vh - 100px); overflow-y: auto;">
-                                            <table id="user-list-table" class="table table-striped" role="grid" data-bs-toggle="data-table">
-                                                <thead>
-                                                    <tr class="ligth">
-                                                        
-                                                        <th>
-                                                         Date
-                                                        </th>
-                                                        <th>
-                                                           Punchin Time
-                                                        </th>
-                                                        <th>
-                                                            Punchout_Time
-                                                        </th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>                 
-                                                    <?php
-                                                      if($_SESSION['e_role']=="user")
-                                                      {
 
-                                                      $q = "select * from attendance where emp_id = '".$_SESSION['user_id']."' " ;
-                                                        $data=mysqli_query($conn,$q);
-                                                        while($row=mysqli_fetch_array($data))
-                                                        {
-                                                            ?>
-                                                        <tr>
-                                                         <td><?php echo $row['c_date']?></td>
-                                                         <td><?php echo $row['punchin_time']?></td>
-                                                        <td><?php echo $row['punchout_time']?></td>               
-                                                        
-                                                            <?php
-                                                        }
-                                                        
-                                                      }
-                                                      elseif($_SESSION['e_role']=="admin")
-                                                      {     
-                                                          $q = "select * from attendance" ;
-                                                          $data=mysqli_query($conn,$q);
-                                                          while($row=mysqli_fetch_array($data))
-                                                          {
-                                                               
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Leaves</title>
+    <!-- <link rel="stylesheet" href="../css/main.min.css">
+        <link rel="stylesheet" href="../sidebar.css"> -->
+</head>
+<?php 
+ $q_emp = "select * from employee" ;
+ $data_emp =mysqli_query($conn,$q_emp);
+ $row_emp=mysqli_fetch_array($data_emp)
+?>
+<body>
+    <main class="main-content">
+        <div class="conatiner-fluid content-inner mt-n5 py-0">
+            <div class="row">
+                <div class="col-xl-12 col-lg-8">
+                    <div class="card">
+                        <div class="card-header d-flex justify-content-between">
+                            <h4 class="card-title">
+                                Attendence List
+                            </h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="card-body px-0">
+                                <div class="table-responsive">
+                                    <div class="sidebar-body pt-0 data-scrollbar" style="max-height: calc(100vh - 100px); overflow-y: auto;">
+                                        <table id="user-list-table" class="table table-striped" role="grid" data-bs-toggle="data-table">
+                                            <thead>
+                                                <tr class="ligth">
+
+                                                    <?php
+                                                    if ($_SESSION['e_role'] == "admin") {
                                                     ?>
-                                                    <tr>
-                                                         <td><?php echo $row['c_date']?></td>
-                                                         <td><?php echo $row['punchin_time']?></td>
-                                                        <td><?php echo $row['punchout_time']?></td>         
-                                                        </tr>
-                                                        <?php  
-                                                        }
+                                                        <th>
+                                                            Employee id
+                                                        </th>
+                                                    <?php
                                                     }
                                                     ?>
-                                                </tbody>
-                                            </table>
-                                        </div>
+                                                    <th>
+                                                        Date
+                                                    </th>
+                                                    <th>
+                                                        Punchin Time
+                                                    </th>
+                                                    <th>
+                                                        Punchout Time
+                                                    </th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <?php
+                                                if ($_SESSION['e_role'] == "user") {
+
+                                                    $q = "select * from attendance where emp_id = '" . $_SESSION['user_id'] . "' ";
+                                                    $data = mysqli_query($conn, $q);
+                                                    while ($row = mysqli_fetch_array($data)) {
+                                                ?>
+                                                        <tr>
+                                                            <td><?php echo $row['c_date'] ?></td>
+                                                            <td><?php echo $row['punchin_time'] ?></td>
+                                                            <td><?php echo $row['punchout_time'] ?></td>
+
+                                                        <?php
+                                                    }
+                                                } elseif ($_SESSION['e_role'] == "admin") {
+                                                    $q = "select * from attendance";
+                                                    $data = mysqli_query($conn, $q);
+                                                    while ($row = mysqli_fetch_array($data)) {
+                                                        ?>
+                                                        <tr>
+                                                            <td><?php echo $row_emp['e_fname'] ." ". $row_emp['e_lname']  ?></td>
+                                                            <td><?php echo $row['c_date'] ?></td>
+                                                            <td><?php echo $row['punchin_time'] ?></td>
+                                                            <td><?php echo $row['punchout_time'] ?></td>
+                                                        </tr>
+                                                <?php
+                                                    }
+                                                }
+                                                ?>
+                                            </tbody>
+                                        </table>
                                     </div>
                                 </div>
                             </div>
-                        </div>                                 
-                    </div>                                
+                        </div>
+                    </div>
                 </div>
             </div>
-        </main>
-    </body>
+        </div>
+    </main>
+</body>
+
 </html>
--- punch/punch.php
+++ punch/punch.php
@@ -4,6 +4,8 @@
 // Database connection
 include '../connection/connection.php';
 $uid = $_SESSION['user_id'];
+
+
 class Employee
 {
     private $name;
@@ -142,21 +144,69 @@ if (isset($_POST['punch_in'])) {
                         </div>
                     </div>
                 <?php
-                }
+                } else {
                 ?>
                     <div class="row">
-                        <div class="col-lg-12">
-                            <div class="row">
-                                <div class="col-lg-12">
-                                    <div class="card">
-                                        <div class="card-body">
+                        <div class="col-lg-3">
 
-                                        </div>
+                            <div class="card h-70">
+                                <div class="card-body">
+                                    <img src="../pictures/team-svgrepo-com.svg" class="mb-2" alt="Team image" height="50px" width="50px">
+                                    <div class="progress-detail">
+                                        <?php
+                                        // Assuming $conn is your database connection
+                                        $qry_employee = "SELECT COUNT(*) AS total_employees FROM employee";
+                                        $data_employee = mysqli_query($conn, $qry_employee);
+                                        $row_employee_data = mysqli_fetch_assoc($data_employee);
+                                        $total_employees = $row_employee_data['total_employees'];
+                                        ?>
+                                        <p class="mb-2">Total Employees</p>
+                                        <h4 class="counter"><?php echo $total_employees; ?></h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+
+                            <div class="card h-70">
+                                <div class="card-body">
+                                    <img src="../pictures/teamwork-svgrepo-com.svg" class="mb-2" alt="Team image" height="50px" width="50px">
+                                    <div class="progress-detail">
+                                        <?php
+                                        // Assuming $conn is your database connection
+                                        $qry_team = "SELECT COUNT(*) AS total_teams FROM team";
+                                        $data_team = mysqli_query($conn, $qry_team);
+                                        $row_team_data = mysqli_fetch_assoc($data_team);
+                                        $total_teams = $row_team_data['total_teams'];
+                                        ?>
+                                        <p class="mb-2">Total Teams</p>
+                                        <h4 class="counter"><?php echo $total_teams; ?></h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+                            <div class="card h-70">
+                                <div class="card-body">
+                                    <img src="../pictures/file-done-2-svgrepo-com.svg" class="mb-2" alt="Team image" height="50px" width="50px">
+                                    <div class="progress-detail">
+                                        <?php
+                                        // Assuming $conn is your database connection
+                                        $qry_projects = "SELECT COUNT(*) AS total_projects FROM project";
+                                        $data_projects = mysqli_query($conn, $qry_projects);
+                                        $row_projects_data = mysqli_fetch_assoc($data_projects);
+                                        $total_projects = $row_projects_data['total_projects'];
+                                        ?>
+                                        <p class="mb-2">Total Projects</p>
+                                        <h4 class="counter"><?php echo $total_projects; ?></h4>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                <?php
+                }
+                ?>
             </div>
         </div>
     </main>
